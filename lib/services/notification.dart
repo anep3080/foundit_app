f62d381980846c94e3c047741359c6e512bb0466
Ahmad Daneil <adwari132@gmail.com>
2025-12-16 09:36:04 +0800

    Add notification.dart

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -1,4 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
+class NotificationService {
+  static final _notifications = FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async{
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const settings = InitializationSettings(android: android);
+
+    await _notifications.initialize(settings);
+
+    await requestPermission();
+  }
+
+  static Future<void> requestPermission() async {
+    if (Platform.isAndroid) {
+      final androidImplementation = _notifications.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>();
+
+      await androidImplementation?.requestNotificationsPermission();
+    }
+    }
+  }
